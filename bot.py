f549c729c5b92df8962c49b207ddb1ab2f237f58
Harishkumar <147156726+whitehatboy005@users.noreply.github.com>
2024-10-13 16:45:32 +0530

    Update and rename Bot_testing.py to bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ def send_test_message():
     url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
     payload = {
         'chat_id': CHAT_ID,
-        'text': 'This is a test message from my bot!'
+        'text': 'Congrats You are Perfectly Configured🥳.'
     }
     try:
         response = requests.post(url, data=payload)
